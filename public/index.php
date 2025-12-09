0792e93a58f8918192a726cd899bb63182f41220
rafcnzo <nanangdorris@gmail.com>
2025-12-09 17:57:17 +0700

    Fix static file routing

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
+
+$uri  = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . $uri;
+
+if ($uri !== '/' && file_exists($file)) {
+    return false;
+}
 define('PROJECT_ROOT', dirname(__DIR__));
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
